1ed57e02f163b6ce31275cdf98a6192698a117cc
Siva Sornaram <mailmeatvignesh7@gmail.com>
2021-10-07 20:40:18 +0530

    added checkboxes for displaying the permissions

---
+++ app/helpers/is-fc.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function isFC(permissionval /*, named*/) {
+      console.log("FC helper called, permissionval : ", permissionval);
+      console.log(permissionval & 8);
+      if (permissionval & 8)  return true;
+      else return false;
+});
